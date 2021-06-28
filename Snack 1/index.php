31f7b5ddbdd32f5389b88597c3a165ba3db48bba
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-28 14:34:45 +0200

    Array del campionato

---
+++ Snack 1/index.php
@@ -0,0 +1,29 @@
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    $partite = [
+        [
+            "Virtus Bologna" => "48";
+            "Varese" => "56":
+        ];
+        [
+            "Virtus Roma" => "60";
+            "Treviso" => "55";
+        ];
+        [
+            "S. C. Gira" => "58";
+            "Trieste" => "40";
+        ];
+        [
+            "Petrarca" => "64";
+            "Partenope" => "75";
+        ];
+        [
+            "Ginnastica Roma" => "74";
+            "Pallacanestro Udine" => "70";
+
+        ];
+    ]
+; ?>
